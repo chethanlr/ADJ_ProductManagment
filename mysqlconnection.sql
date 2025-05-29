209a83a0ec73e893ad14a8d6620417166323d8f1
Chethan L R <153184698+chethanlr@users.noreply.github.com>
2025-05-29 21:03:27 +0530

    Update mysqlconnection.sql

--- mysqlconnection.sql
+++ mysqlconnection.sql
@@ -1,1 +1,12 @@
+CREATE DATABASE inventory;
+
+USE inventory;
+
+CREATE TABLE Products (
+    ProductID INT PRIMARY KEY,
+    ProductName VARCHAR(100),
+    Category VARCHAR(50),
+    Price DOUBLE CHECK (Price >= 0),
+    Quantity INT CHECK (Quantity >= 0)
+);
 
